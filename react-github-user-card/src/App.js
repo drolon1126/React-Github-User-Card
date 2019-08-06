0176216bc0858e2fd994e6a6430ded8680c408b6
danRolon <daniel.rolon@yahoo.com>
2019-08-06 12:12:20 -0500

    Innitial Commit - mvp

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import UserCard from './components/UserCard';
+import './App.css';
+import FollowerList from './components/FollowerList';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      userData: {},
+      followerList: []
+    }
+  }
+  componentDidMount(){
+    fetch('https://api.github.com/users/drolon1126')
+      .then(res=>res.json())
+      .then(user=>this.setState({userData: user}))
+      .catch(err => console.log('oops: ', err));
+
+    fetch(`https://api.github.com/users/${this.state.userData.login}/followers`)
+      .then(res=>res.json())
+      .then(followList=>this.setState({followerList: followList}))
+      .catch(err => console.log('oops: ', err));
+      
+  }
+  render(){
+    return(
+      <>
+      <div>
+        <h1>The Current User</h1>
+        <UserCard user={this.state.userData} />
+      </div>
+      <div>
+        <h2>List of Followers</h2>
+        <FollowerList followerList={this.state.followerList} />
+      </div>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ react-github-user-card/src/components/UserCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+
+
+class UserCard extends React.Component {
+  constructor(){
+    super();
+
+  }
+  render(){
+    return(
+      <Card className='card'>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="avatar" className='avatar' src={this.props.user.avatar_url}/>
+          }
+          title={this.props.user.name}
+          subheader={this.props.user.login}
+        />
+        <CardContent>
+        <Typography variant="body1" color="textSecondary" component="h3">
+            Email: {this.props.user.email ? this.props.user.email : 'No email provided'}
+          </Typography>
+          <Typography variant="body1" color="textSecondary" component="h3">
+            Location: {this.props.user.location ? this.props.user.location : 'No location provided'}
+          </Typography>
+          <Typography variant="body1" color="textSecondary" component="h3">
+            Company: {this.props.user.company ? this.props.user.company : 'No Company provided'}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {this.props.user.bio ? this.props.user.bio : 'This user has no bio'}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <a href={this.props.user.html_url}>Github Page </a>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+export default UserCard;
